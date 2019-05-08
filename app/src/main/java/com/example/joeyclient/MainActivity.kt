8790efa234946eecf43dd4fd460d539b1a149118
Kyle Zhou <kylezhou2002@hotmail.com>
2019-05-07 21:01:10 -0700

    Initial commit

---
+++ app/src/main/java/com/example/joeyclient/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.joeyclient
+
+import android.hardware.usb.UsbDevice
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.hardware.usb.UsbManager
+
+class MainActivity : AppCompatActivity() {
+
+    val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
